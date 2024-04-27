f0e43f4e8133838796e5cc295a540c0bf4560972
gkkangk@gmail.com <gkkangk@gmail.com>
2024-04-27 16:08:56 +0300

    Interaction System Progress

--- Assets/Scripts/Runtime/Controllers/PlayerInteractController.cs
+++ Assets/Scripts/Runtime/Controllers/PlayerInteractController.cs
@@ -1,9 +1,33 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 
 namespace Runtime.Controllers
 {
     public class PlayerInteractController : MonoBehaviour
     {
+        [SerializeField] private CinemachineVirtualCamera cam;
+        [SerializeField] private float interactDistance = 5f;
+        [SerializeField] private LayerMask interactLayerMask;
         
+        [SerializeField] private PlayerUIController playerUIController;
+
+        private void Update()
+        {
+            playerUIController.UpdateUI("");
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.red);
+            
+            RaycastHit hit; 
+            if (Physics.Raycast(ray, out hit, interactDistance, interactLayerMask))
+            {
+                if(hit.collider.GetComponent<Interactable>() != null)
+                {
+                   playerUIController.UpdateUI(hit.collider.GetComponent<Interactable>().promptMessage);
+                }
+            }
+                
+            
+        }
     }
 }
