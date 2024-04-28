08afdd7106dae14d21519234fda7cf38dc0f7900
ozgurd5 <ozgurdalbeler@gmail.com>
2024-04-28 18:02:27 +0300

    Player Interactable and Camera Bug Fix

--- Assets/Scripts/Runtime/Utilities/DoorInteractable.cs
+++ Assets/Scripts/Runtime/Utilities/DoorInteractable.cs
@@ -1,26 +1,18 @@
-﻿using Cysharp.Threading.Tasks;
-using System;
-using UnityEngine;
+﻿using System;
 
 public class DoorInteractable : Interactable
 {
-    DoorOpen doorOpen;
-
-    public event Action OnInteracted;
-    public bool isDeviceInstalled { get; private set; } = false;    
-
-    [SerializeField] float interactDuration = 3f;
+    public bool isDeviceInstalled;
+    private DoorOpen doorOpen;
 
     private void Start()
     {
         doorOpen = GetComponent<DoorOpen>();
         doorOpen.InitializeDoor(this);
     }
-    protected async override void Interact()
+
+    protected override void Interact()
     {
-        //base.Interact();
-        OnInteracted?.Invoke();
-        await UniTask.WaitForSeconds(interactDuration);
         isDeviceInstalled = true;
     }
 }
