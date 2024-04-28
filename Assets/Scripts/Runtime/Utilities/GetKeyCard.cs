0026c358456615336c157fe48817bdd6537b76af
ozgurd5 <ozgurdalbeler@gmail.com>
2024-04-28 19:48:15 +0300

    Fuckign Fix

--- Assets/Scripts/Runtime/Utilities/GetKeyCard.cs
+++ Assets/Scripts/Runtime/Utilities/GetKeyCard.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Runtime.Abstracts;
+using UnityEngine;
 
 public class GetKeyCard : Interactable
 {
--- Assets/Scripts/Runtime/Utilities/SetKeyCard.cs
+++ Assets/Scripts/Runtime/Utilities/SetKeyCard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Runtime.Abstracts;
 using UnityEngine;
 
 public class SetKeyCard : Interactable
