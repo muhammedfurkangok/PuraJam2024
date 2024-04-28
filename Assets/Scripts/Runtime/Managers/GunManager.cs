0f748dabdc7bf3f3ce3606bb3ac74f098a276351
gkkangk@gmail.com <gkkangk@gmail.com>
2024-04-28 18:26:55 +0300

    Bug Fix

--- Assets/Scripts/Runtime/Managers/GunManager.cs
+++ Assets/Scripts/Runtime/Managers/GunManager.cs
@@ -13,6 +13,7 @@ public class GunManager : NetworkBehaviour
     public float impactForce = 30f;
     public float fireRate = 15f;
     public float nextTimeToFire = 0f;
+    [SerializeField] private float recoil;
 
     [SerializeField] private Transform bulletSpawn;
     [SerializeField] private CinemachineVirtualCamera cam;
@@ -46,9 +47,20 @@ public class GunManager : NetworkBehaviour
         GameObject bulletGO = Instantiate(bullet, bulletSpawn.position, cam.transform.rotation);
         bulletGO.GetComponent<Rigidbody>().AddForce(cam.transform.forward * 1000f);
         NetworkServer.Spawn(bulletGO);
-
+        MoveGunBackward();
         await UniTask.WaitForSeconds(2f);//
         Destroy(bulletGO);
         NetworkServer.Destroy(bulletGO);
+        ResetGunPosition();
+    }
+    private void MoveGunBackward()
+    {
+        // Move the gun slightly backward in local space
+        transform.Translate(Vector3.back * recoil); // Adjust the distance as needed
+    }
+    private void ResetGunPosition()
+    {
+        // Reset the gun position to its original position
+        transform.localPosition = Vector3.Lerp(transform.position, new Vector3(0, -26.49f, 0),0.2f);
     }
 }
